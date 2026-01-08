f4c12c80ec07b330ea221c5548cca8757bfa02e5
Adarshpatel0308 <adarshpatel88899@gmail.com>
2026-01-08 00:35:02 -0500

    Add secrets in repo

---
+++ scripts/install-jenkins.sh
@@ -0,0 +1,39 @@
+
+#!/bin/bash
+set -e
+
+sudo apt update -y
+sudo apt install -y fontconfig openjdk-17-jre git curl unzip
+
+# Add Jenkins repo
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+sudo apt update -y
+sudo apt install -y jenkins
+
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+
+# Install Terraform
+TF_VERSION="1.9.5"
+curl -LO https://releases.hashicorp.com/terraform/${TF_VERSION}/terraform_${TF_VERSION}_linux_amd64.zip
+unzip terraform_${TF_VERSION}_linux_amd64.zip
+sudo mv terraform /usr/local/bin/
+
+# Install AWS CLI
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+# Install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s \
+  https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
+
+echo "Jenkins Installation Completed Successfully!"
